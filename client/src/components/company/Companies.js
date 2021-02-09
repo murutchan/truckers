6d314d7953e0cdcdc41d8b1bf4449a0602a2bda0
Askar Talantbek <murutchan@gmail.com>
2021-02-09 09:27:47 -0600

    company card

--- client/src/components/company/Companies.js
+++ client/src/components/company/Companies.js
@@ -4,25 +4,38 @@ import PropTypes from "prop-types";
 import { getAllCompanies } from "../../actions/company";
 import { addLike } from "../../actions/company";
 import { removeLike } from "../../actions/company";
+import "./company.styles.scss";
 
 const Companies = ({ companies, getAllCompanies, addLike, removeLike }) => {
   useEffect(() => {
     getAllCompanies();
-    console.log(companies);
   }, [getAllCompanies]);
-  console.log(companies);
 
   return (
-    <div className="container ">
+    <div className="container">
       {companies.map((company) => (
-        <div className="w-50 mb-3  bg-info border">
-          <h1 className="text-light">Company: {company.companyName}</h1>
-          <p className="text-danger"> phoneNumber: {company.phone} </p>
-          <p> address: {company.address} </p>
-          <p> City: {company.city} </p>
-          <p> State: {company.state} </p>
+        <div className=" container mb-3 border text-center">
+          <div className="row mx-auto">
+            <h2 className="text-success mt-3 mx-auto">{company.companyName}</h2>
+          </div>
+          <div className="row">
+            <div className="col-md-6">
+              <p>
+                <i class="fas fa-map-marked-alt mr-2 text-info"></i>
+                {company.address}, {company.city},
+                {company.state && company.state},{company.zip}{" "}
+              </p>
+            </div>
+            <div className="col-md-6">
+              <p>
+                <i class="fas fa-phone-square-alt mr-2 text-info"></i>
+                {company.phone}
+              </p>
+            </div>
+          </div>
+
           <button
-            className="btn btn-light btn-sm"
+            className="btn btn-light btn-sm mb-3"
             onClick={(e) => addLike(company._id)}
           >
             <i class="fas fa-thumbs-up"></i>{" "}
@@ -31,8 +44,7 @@ const Companies = ({ companies, getAllCompanies, addLike, removeLike }) => {
             )}
           </button>
           <button
-            type="button"
-            class="btn btn-light"
+            class="btn btn-light btn-sm mb-3"
             onClick={() => removeLike(company._id)}
           >
             <i class="fas fa-thumbs-down"></i>
